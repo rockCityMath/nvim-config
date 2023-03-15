960226ada4b1193420366b60f85e47ac203def70
zsc0012 <zacharycarpenter@my.unt.edu>
2023-03-15 09:50:34 -0500

    init

---
+++ lua/rockcity/packer.lua
@@ -0,0 +1,69 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Install by running :PackerSync
+-- Usually have to add a config in the after/plugins folder
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Telescope
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use('sharkdp/fd')
+
+  -- Rose Pine Theme
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  vim.cmd('colorscheme rose-pine')
+use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+    }
+  end
+}
+
+
+  -- Small Plugins
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  use("nvim-treesitter/nvim-treesitter-context");
+  use("tmsvg/pear-tree")
+  use('ray-x/lsp_signature.nvim')
+
+  -- LSP Zero
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v1.x',
+  requires = {
+	  -- LSP Support
+	  {'neovim/nvim-lspconfig'},             -- Required
+	  {'williamboman/mason.nvim'},           -- Optional
+	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	  -- Autocompletion
+	  {'hrsh7th/nvim-cmp'},         -- Required
+	  {'hrsh7th/cmp-nvim-lsp'},     -- Required
+	  {'hrsh7th/cmp-buffer'},       -- Optional
+	  {'hrsh7th/cmp-path'},         -- Optional
+	  {'saadparwaiz1/cmp_luasnip'}, -- Optional
+	  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+	  -- Snippets
+	  {'L3MON4D3/LuaSnip'},             -- Required
+	  {'rafamadriz/friendly-snippets'}, -- Optional
+  }
+  }
+
+end)
